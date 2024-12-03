0c9bfad0b730c1fda1f778358f79e7efa0bcfb4b
kihono2817 <kihono2817@nestvia.com>
2024-12-03 16:05:20 +0800

    feat : add landing page and update routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,12 +10,13 @@ export const routes: Routes = [
     path: 'home',
     loadComponent: () => import('./home/home.page').then(m => m.HomePage)
   },
-  // {
-  //   path: 'landing',
-  //  loadComponent: () => import('./landing/landing.page').then(m => m.LandingPage)
-  // },
   {
     path: 'login',
     loadComponent: () => import('./login/login.page').then(m => m.LoginPage)
   },
+  {
+    path: 'landing',
+    loadComponent: () => import('./landing/landing.page').then( m => m.LandingPage)
+  },
+
 ];
