4daa5fb3eac31f7e8afa032c3314f0d3f8e3c93e
jefefefef <paguilajeferson@gmail.com>
2024-12-03 15:57:53 +0800

    update : routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,19 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'login',
+    redirectTo: 'home',
     pathMatch: 'full',
   },
   {
+    path: 'home',
+    loadComponent: () => import('./home/home.page').then(m => m.HomePage)
+  },
+  // {
+  //   path: 'landing',
+  //  loadComponent: () => import('./landing/landing.page').then(m => m.LandingPage)
+  // },
+  {
     path: 'login',
-    loadComponent: () => import('./login/login.page').then( m => m.LoginPage)
+    loadComponent: () => import('./login/login.page').then(m => m.LoginPage)
   },
 ];
