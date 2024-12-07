849a3e9da10ede6e1ffd30dc803799f06c80887a
jefefefef <paguilajeferson@gmail.com>
2024-12-07 08:15:11 +0800

    Implement home page design and fix ngFor directive issue

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardHeader, IonCardTitle, IonButton, IonFooter } from '@ionic/angular/standalone';
+import { CommonModule } from '@angular/common'; // Import this
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardHeader, IonCardTitle, IonButton, IonFooter, IonChip, IonGrid, IonRow, IonCol } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonFooter, IonButton, IonCardTitle, IonCardHeader, IonCard, IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonCol, IonRow, IonGrid, IonChip, 
+    CommonModule, // Add this to support NgFor
+    IonFooter,
+    IonButton,
+    IonCardTitle,
+    IonCardHeader,
+    IonCard,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+  ],
 })
 export class HomePage {
-  constructor() {}
+  products = [
+    { name: 'Product 1', price: 100, image: 'assets/product1.jpg' },
+    { name: 'Product 2', price: 200, image: 'assets/product2.jpg' },
+    { name: 'Product 3', price: 300, image: 'assets/product3.jpg' },
+  ];
 }
