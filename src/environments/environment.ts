03c9cad9b68b8007a311d54cb34417e63b1f99a6
mrndstvndv <stvndvmrnd@gmail.com>
2025-04-01 17:45:24 +0800

    ci: add warn in firebaseconfig

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,18 +2,10 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-const env = process.env
-
 export const environment = {
   production: false,
-  firebaseConfig: {
-    apiKey: env['FIREBASE_API_KEY'],
-    authDomain: env['FIREBASE_AUTH_DOMAIN'],
-    projectId: env['FIREBASE_PROJECT_ID'],
-    storageBucket: env['FIREBASE_STORAGE_BUCKET'],
-    messagingSenderId: env['FIREBASE_MESSAGING_SENDER_ID'],
-    appId: env['FIREBASE_APP_ID']
-  }
+  // WARN: add your firebase config here
+  firebaseConfig: {}
 };
 
 /*
