eff2f60c1b90e2ef13c9142795d2c6a56b256272
mrndstvndv <stvndvmrnd@gmail.com>
2025-04-01 21:09:02 +0800

    fix: update guard to allow opening login page when not logged in

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -3,6 +3,7 @@ import { Auth, user } from '@angular/fire/auth';
 import { CanActivateFn, Router } from '@angular/router';
 import { map } from 'rxjs';
 
+// WARN: this does not work, I mean it works but injection is not working
 export const authGuard: CanActivateFn = (route, state) => {
   const auth: Auth = inject(Auth);
   const router: Router = inject(Router);
@@ -12,11 +13,15 @@ export const authGuard: CanActivateFn = (route, state) => {
 
   return user$.pipe(
     map((user) => {
-      if (user) {
-        if (currentRouteIsLogin) {
-          router.navigate(['/home'])
-          return false
-        }
+
+      if (user && currentRouteIsLogin) {
+        router.navigate(['/home'])
+        return false
+      }
+      else if (currentRouteIsLogin) {
+        return true
+      }
+      else if (user) {
         return true;
       } else {
         router.navigate(['/login'])
