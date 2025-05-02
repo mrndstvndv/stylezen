580fe4ac95751ac14620b97380843df631fb96e0
mrndstvndv <stvndvmrnd@gmail.com>
2025-05-02 23:36:14 +0800

    remove creds

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,5 @@
 export const environment = {
   production: true,
-  firebaseConfig: {
-    apiKey: "AIzaSyBJhr_mbEXrEwMqWCNxCXoSqSsU9-PaIQ0",
-    authDomain: "stylezen-5d403.firebaseapp.com",
-    projectId: "stylezen-5d403",
-    storageBucket: "stylezen-5d403.firebasestorage.app",
-    messagingSenderId: "511168785207",
-    appId: "1:511168785207:web:cd68323f8b67e5cb0248b2"
-  }
+  // WARN: add your creds
+  firebaseConfig: {}
 };
